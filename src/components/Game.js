a9a65ba0e35a76a0b7bd9ffd7504ccfb2a07e5bd
Eric <eric@webdevdoyle.com>
2018-11-13 23:04:04 +0100

    todo lifecycle logic noted 25/Game.js

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,31 +1,18 @@
 import React, { Component } from 'react';
 import NavBar from './NavBar';
-import '../styles/Game.css'
 import Box from './Box';
 import PropTypes from 'prop-types';
-
-// const Game = ({colors, newGame, appTitle}) => {
-//   const boxes = colors.map((col, i) => (
-//       <Box
-//       className="Box"
-//       key={i}
-//       color={col} />
-//   ));
-//   return(
-//     <div className="Game">
-//       <NavBar newGame={newGame} appTitle={appTitle}/>
-//       <div className="Game__container">
-//         <div className="Game__box-container">
-//           {boxes}
-//         </div>
-//       </div>
-//     </div>
-//   )
-// }
-//
-// export default Game;
+import '../styles/Game.css';
 
 class Game extends Component {
+
+  constructor(props){
+    super(props);
+    this.state = {
+      visibleIds: []
+    }
+    this.addVisibleId = this.addVisibleId.bind(this);
+  }
 
   static propTypes = {
     colors: PropTypes.array,
@@ -33,12 +20,27 @@ class Game extends Component {
     appTitle: PropTypes.string
   }
 
+  addVisibleId(id){
+    this.setState({visibleIds: [...this.state.visibleIds, id]});
+    //adding didmou/didupdate logic here and then validate for visibleIds>=2
+    //and setTimeout for clearing visibleIds
+  }
+
   render() {
       const boxes = this.props.colors.map((col, i) => (
+        this.state.visibleIds.indexOf(i) !== -1 ?
           <Box
-          className="Box"
           key={i}
-          color={col} />
+          index={i}
+          color={col}
+          addVisId={this.addVisibleId}
+          visIds={this.state.visibleIds} />
+        :
+          <Box
+          key={i}
+          index={i}
+          addVisId={this.addVisibleId}
+          visIds={this.state.visibleIds} />
       ));
       return(
         <div className="Game">
